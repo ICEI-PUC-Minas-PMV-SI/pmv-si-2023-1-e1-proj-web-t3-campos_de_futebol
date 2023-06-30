4028cba546102e28a4ab0f0b793223af0d22eaca
Eduardo-medeiros <128246244+Eduardo-medeiros@users.noreply.github.com>
2023-06-30 19:20:02 -0300

    Update cadastrodecampo.js
    acho que eu n mudei nada, mas so para ter certeza q ta atualizado msm

--- src/FOOTBALL_MAPS/cadastroDeCampo/cadastrodecampo.js
+++ src/FOOTBALL_MAPS/cadastroDeCampo/cadastrodecampo.js
@@ -5,6 +5,7 @@ function enviarFormulario() {
   var localizacao = document.querySelector('textarea[name="localizacao"]').value;
   var contato = document.querySelector('textarea[name="MeiosDeContato"]').value;
 
+
   if (campoImagem === '' || titulo === '' || descricao === '' || localizacao === '' || contato === '') {
     alert('Por favor, preencha todos os campos obrigatórios.');
     return;
@@ -16,6 +17,7 @@ function enviarFormulario() {
     localizacao: localizacao,
     contato: contato
   };
+
 
   fetch('http://localhost:3000/campos', {
     method: 'POST',
@@ -34,15 +36,19 @@ function enviarFormulario() {
   });
 }
 
+
 function exibirMensagem(mensagem) {
   var mensagemElement = document.getElementById('mensagem');
   mensagemElement.textContent = mensagem;
 }
 
+
 var botaoCriar = document.getElementById('botaoEnviar');
 botaoCriar.addEventListener('click', enviarFormulario);
 
+
 let proximoId = 1;
+
 
 function gerarNovoId() {
   const novoId = proximoId;
